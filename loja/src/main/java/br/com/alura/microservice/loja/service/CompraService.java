6742d38d2d6cb2b6fc796fd3bb6700a1dd5e99f8
Victor Hugo <victorhugolgr@gmail.com>
2020-06-15 21:10:26 -0300

    Microservico com spring cloud

---
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -0,0 +1,50 @@
+package br.com.alura.microservice.loja.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import br.com.alura.microservice.loja.client.FornecedorClient;
+import br.com.alura.microservice.loja.dto.CompraDTO;
+import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
+import br.com.alura.microservice.loja.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.model.Compra;
+
+@Service
+public class CompraService {
+	
+	@Autowired
+	private FornecedorClient fornecedorClient;
+	
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback")
+	public Compra realizaCompra(CompraDTO compra) {
+		
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		
+		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
+		
+		System.out.println(info.getEndereco());
+		
+		Compra compraSalva = new Compra();
+		compraSalva.setPedidoId(pedido.getId());
+		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
+		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return compraSalva;
+	}
+	
+	public Compra realizaCompraFallback(CompraDTO compra) {
+		Compra compraFallback = new Compra();
+		compraFallback.setEnderecoDestino(compra.getEndereco().toString());
+		return compraFallback;
+	}
+	
+}
