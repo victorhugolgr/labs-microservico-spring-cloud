5935d22419ae6fe66429b11b29015943f5663412
Victor Hugo <victorhugolgr@gmail.com>
2020-06-24 08:28:08 -0300

    configuration docker-compose

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mvn clean package -f ./auth/pom.xml -DskipTests & \ 
+mvn clean package -f ./config-server/pom.xml -DskipTests & \ 
+mvn clean package -f ./eureka-server/pom.xml -DskipTests & \
+mvn clean package -f ./fornecedor/pom.xml -DskipTests & \
+mvn clean package -f ./loja/pom.xml -DskipTests & \
+mvn clean package -f ./transportador/pom.xml -DskipTests & \
+mvn clean package -f ./zuul/pom.xml -DskipTests
+
+wait
+
+docker-compose build
+docker-compose up
